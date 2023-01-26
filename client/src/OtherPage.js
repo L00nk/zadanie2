f96ea4e917892101ecee2cdd8e0771dc250aa24c
L00nk <agata.wrzesniewska1@gmail.com>
2023-01-26 20:05:37 +0100

    Initial commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      1. Multifib 2. Agata Wrześniewska 3. I2S 1.5
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
